e8daedffccf68e3a321a477ff84506b1f3b0e98e
Perpetue <muperpety@gmail.com>
2019-01-11 11:15:14 +0200

    Prototype to Bank points in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -27,4 +27,45 @@ function Player(name) {
     return roll;
   }
   
+  // Current Turn Array Prototype
+  Player.prototype.addRollToArray = function (x) {
+    this.currentTurnArray.push(x)
+  }
+  
+  // Get sum of Turn Running Total (and exclude 1s from total)
+  Player.prototype.sumOfRolls = function() {
+    for (var i = 0; i < this.currentTurnArray.length; i++) {
+      if (this.currentTurnArray[i = this.currentTurnArray.length - 1] === 1) {
+        this.turnRunningScore = this.turnRunningScore;
+      } else {
+        this.turnRunningScore += this.currentTurnArray[i = this.currentTurnArray.length - 1];
+      }
+    }
+  }
+  // Prototype to Bank points
+  Player.prototype.bankPoints = function() {
+    this.totalBankedScore += this.turnRunningScore;
+  }
+  
+  // Hold/Stay turnRunningScore <--Added this, score will bank when a user presses stay
+  Player.prototype.stayTurn = function () {
+    this.bankPoints();
+    this.turnRunningScore = 0;
+  }
+  
+  // Create six-sided dice
+  var sixSidedDice = new Dice();
+  
+  //Prototype to enter last roll value into player object
+  Player.prototype.setLastRoll = function(x) {
+    this.lastRoll = x;
+  }
+  
+  // Prototype to reset running total if 1 is rolled
+  Player.prototype.resetRunningTotalOnOne = function() {
+    if (this.lastRoll === 1) {
+      this.turnRunningScore = 0;
+    }
+  }
+  
   
