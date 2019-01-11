4c6166aa6469946d25b8cff0586cde46c5c91c2e
Perpetue <muperpety@gmail.com>
2019-01-11 11:08:03 +0200

    add Player Object Constructor in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business logic
+
+//Player Object Constructor
+function Player(name) {
+    this.name = name;
+    this.turnRunningScore = 0;
+    this.totalBankedScore = 0;
+    this.lastRoll = 0;
+    this.currentTurnArray = [];
+  }
+  
+  
